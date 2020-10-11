0df307ba17c49ae5e87f80941b19e495bbff204c
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-11 20:38:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,12 @@
 # code your #valid_move? method here
 def valid_move?()
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
   
 end
 
