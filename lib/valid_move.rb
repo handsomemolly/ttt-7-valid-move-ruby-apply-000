95c169ea2e09c45dce434381b25fa9051f91d67e
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-11 20:52:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,21 +7,5 @@ def valid_move?(board, index)
     end
   end
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
-
-
-end
-
-
+  def on_board(num)
+    if num.between?(0, 8) ==
